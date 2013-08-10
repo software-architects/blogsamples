58236114dd48102bb7d549f6b0cafbaa7889dd5d
Simon Opelt <simon@software-architects.at>
2013-08-10 20:16:44 +0200

    initial version from blog article
    see
    http://www.software-architects.com/devblog/2012/10/29/appconfig-in-IronPython-without-additional-assemblies

---
+++ UseLibraryWithAppConfig/SomeLibrary/Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace SomeLibrary
+{
+	using System.Configuration;
+
+	/// <summary>
+	/// Example helper class.
+	/// </summary>
+	public static class Helper
+    {
+		/// <summary>
+		/// Gets a string which contains some data from appSettings.
+		/// </summary>
+		/// <returns>Some string including config data.</returns>
+		public static string GetSomeValueUsingConfig()
+		{
+			var someSetting = ConfigurationManager.AppSettings["SomeSetting"];
+			return string.Format("The config contains: {0}", someSetting);
+		}
+    }
+}
