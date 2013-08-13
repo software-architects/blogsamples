f66f0426e8accce5833f7ce957f2df9163189670
Simon Opelt <simon@software-architects.at>
2013-08-13 11:28:52 +0200

    cleanup, readme

---
+++ UseLibraryWithAppConfig/README.md
@@ -0,0 +1,4 @@
+system_configuration
+====================
+This short sample shows how to use .NET libraries which use System.Configuration by redirecting to a custom config file.
+For more information see [our blog post](http://www.software-architects.com/devblog/2012/10/29/appconfig-in-IronPython-without-additional-assemblies) or [the stackoverflow question](http://stackoverflow.com/q/550723/468244) related to the use-case.
---
+++ UseLibraryWithAppConfig/UseLibraryWithAppConfig/system_configuration.py
@@ -0,0 +1,43 @@
+import clr
+clr.AddReference("System.Configuration")
+from System.Configuration.Internal import IInternalConfigSystem
+class ConfigurationProxy(IInternalConfigSystem):
+    def __init__(self, fileName):
+        from System import String
+        from System.Collections.Generic import Dictionary
+        from System.Configuration import IConfigurationSectionHandler, ConfigurationErrorsException
+        self.__customSections = Dictionary[String, IConfigurationSectionHandler]()
+        loaded = self.Load(fileName)
+        if not loaded:
+            raise ConfigurationErrorsException(String.Format("File: {0} could not be found or was not a valid cofiguration file.", fileName))
+
+    def Load(self, fileName):
+        from System.Configuration import ExeConfigurationFileMap, ConfigurationManager, ConfigurationUserLevel
+        exeMap = ExeConfigurationFileMap()
+        exeMap.ExeConfigFilename = fileName
+        self.__config = ConfigurationManager.OpenMappedExeConfiguration(exeMap, ConfigurationUserLevel.None);
+        return self.__config.HasFile;
+    
+    def GetSection(self, configKey):
+        if configKey == "appSettings":
+            return self.__BuildAppSettings()
+        return self.__config.GetSection(configKey);
+    
+    def __BuildAppSettings(self):
+        from System.Collections.Specialized import NameValueCollection
+        coll = NameValueCollection()
+        for key in self.__config.AppSettings.Settings.AllKeys:
+            coll.Add(key, self.__config.AppSettings.Settings[key].Value);
+        return coll
+
+    def RefreshConfig(self, sectionName):
+        self.Load(self.__config.FilePath)
+        
+    def SupportsUserConfig(self):
+        return False
+    
+    def InjectToConfigurationManager(self):
+        from System.Reflection import BindingFlags
+        from System.Configuration import ConfigurationManager
+        configSystem = clr.GetClrType(ConfigurationManager).GetField("s_configSystem", BindingFlags.Static | BindingFlags.NonPublic)
+        configSystem.SetValue(None, self);
